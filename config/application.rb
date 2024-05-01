41b58fb4570e7666d649250c74a1e8cb31fa928a
yamazaki yuri <hi54.1olbooo3@gmail.com>
2024-05-01 16:13:16 +0900

    トップページの作成

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,18 @@ module Myapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    config.generators do |g|
+      g.skip_routes true      # Skip routes.rb file update
+      g.helper false          # Don't create helper files
+      g.test_framework :rspec, # Set RSpec as test framework
+                      controller_specs: true,  # Enable controller specs
+                      fixtures: false,         # Disable fixtures
+                      helper_specs: false,     # Disable helper specs
+                      model_specs: true,       # Enable model specs
+                      request_specs: false,    # Disable request specs
+                      routing_specs: false, # Disable routing specs
+                      view_specs: false # Disable view specs
+    end
   end
 end
