b2c14c5856a85a64efbe1c0e60089ae0dc625c11
yamazaki yuri <hi54.1olbooo3@gmail.com>
2024-05-04 15:21:58 +0900

    googleログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :sns_credentials, dependent: :destroy
+  
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable,
+        :omniauthable, omniauth_providers: %i[google_oauth2]
+
+  def self.from_omniauth(auth)
+    sns_credential = SnsCredential.find_by(provider: auth.provider, uid: auth.uid)
+    if sns_credential
+      return sns_credential.user
+    else
+      user = User.find_or_create_by(email: auth.info.email) do |u|
+        u.name = auth.info.name
+        u.password = Devise.friendly_token[0, 20]
+      end
+      user.sns_credentials.create(provider: auth.provider, uid: auth.uid)
+      return user
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
   }
   root 'tops#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
