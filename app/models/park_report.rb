8d3adb2bd6b845e326a5183df7e021b56aecbe6c
yamazaki yuri <hi54.1olbooo3@gmail.com>
2024-05-14 16:27:14 +0900

    投稿の削除

--- app/models/park_report.rb
+++ app/models/park_report.rb
@@ -2,7 +2,7 @@ class ParkReport < ApplicationRecord
   belongs_to :user
   belongs_to :park
   belongs_to :tokyo_ward
-  has_many :report_images
+  has_many :report_images, dependent: :destroy
   accepts_nested_attributes_for :report_images
 
   validates :title, presence: true
