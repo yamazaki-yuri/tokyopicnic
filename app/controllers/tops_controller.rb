63aaf85a7d78ef1b6a7629f711ce2b9e7d788823
yamazaki yuri <hi54.1olbooo3@gmail.com>
2024-05-18 23:39:32 +0900

    公園をランダムで表示する

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,4 +1,8 @@
 class TopsController < ApplicationController
   def index
+    random_park = Park.pluck(:id).sample
+    @park = Park.find(random_park)
+    @park_images = @park.park_images
+    @park_tokyo_ward = @park.tokyo_wards.first
   end
 end
