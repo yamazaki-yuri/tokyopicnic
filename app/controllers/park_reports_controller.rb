c9c95d45a55f5a1982e7de7c26f79d6e2f1c013b
yamazaki yuri <hi54.1olbooo3@gmail.com>
2024-05-21 15:40:20 +0900

    新規投稿の機能編集

--- app/controllers/park_reports_controller.rb
+++ app/controllers/park_reports_controller.rb
@@ -5,28 +5,27 @@ class ParkReportsController < ApplicationController
   
   def new
     @park_report = ParkReport.new
-    2.times { @park_report.report_images.build }
+    @park_report.report_images.build
   end
 
   def create
+    @park_report = ParkReport.new(recieve_params)
     @park_name = params[:park_report][:park_name]
     @park = Park.find_by(name: @park_name)
 
     if @park
       @tokyo_ward = @park.tokyo_wards.first
     else
-      if params[:park_report][:tokyo_ward_id].present?
-        @tokyo_ward = TokyoWard.find(params[:park_report][:tokyo_ward_id])
-      else
-        flash[:danger] = "区を選択してください"
-        render :new, status: :unprocessable_entity
-        return
-      end
-
-      create_park_and_associate_tokyo_ward
+      receive_tokyo_ward
     end
 
-    save_park_report
+    if @park.nil?
+      flash.now[:danger] = "指定された公園が見つかりませんでした。東京23区にある公園を入力してください。"
+      @park_report.park_name = @park_name
+      @park_report.tokyo_ward_id = params[:park_report][:tokyo_ward_id]
+    else
+      save_park_report
+    end
   end
 
   def edit; end
@@ -68,12 +67,27 @@ class ParkReportsController < ApplicationController
     @park_report = ParkReport.includes(:report_images, :park).find(params[:id])
   end
 
+  def recieve_params
+    params.require(:park_report).permit(:park_name, :tokyo_ward_id, :date, :title, :comment, report_images_attributes: [:url])
+  end
+
+
   def report_params
     params.require(:park_report).permit(:date, :title, :comment, report_images_attributes: [:url]).merge(park_id: @park.id, tokyo_ward_id: @tokyo_ward.id)
   end
 
   def edit_params
     params.require(:park_report).permit(:date, :title, :comment)
+  end
+
+  def receive_tokyo_ward
+    if params[:park_report][:tokyo_ward_id].present?
+      @tokyo_ward = TokyoWard.find(params[:park_report][:tokyo_ward_id])
+      create_park_and_associate_tokyo_ward
+    else
+      flash.now[:danger] = "区を選択してください"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def save_park_report
@@ -96,11 +110,15 @@ class ParkReportsController < ApplicationController
     response = google_places_service.search(location, @park_name)
 
     park_info = response['results'].first
+      if park_info.nil?
+        flash[:danger] = "公園が東京23区にないか、区が間違っています。"
+        render :new, status: :unprocessable_entity
+        return
+      end
     place_id = park_info['place_id']
-
     web_response = google_places_service.get_website(place_id)
     website = web_response['result']['website']
-    
+  
     @park = Park.create(name: @park_name, googlemaps_place_id: place_id, website_url: website)
     @park.tokyo_wards << @tokyo_ward
   end
