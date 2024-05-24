851cb16f40ed76e6354f6abed58fa48bc4d8bbec
yamazaki yuri <hi54.1olbooo3@gmail.com>
2024-05-24 16:15:53 +0900

    お問い合わせページの作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def page_title(page_title = '', admin: false)
-    base_title = 'TokyoPicnic'
+    base_title = '東京Picnic'
 
-    page_title.empty? ? base_title : "#{page_title} | #{base_title}"
+    page_title.empty? ? base_title : "#{page_title} - #{base_title}"
   end
 end
