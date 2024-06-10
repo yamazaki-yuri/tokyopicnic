ee06e4f0e1e5d973bf9c6f15880d15913774b0c6
yamazaki yuri <hi54.1olbooo3@gmail.com>
2024-06-10 17:27:54 +0900

    ドメイン名の設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.hosts << 'https://tokyopicnic.com'
+  config.hosts << 'tokyopicnic.com'
   host = 'https://tokyopicnic.com/'
   config.action_mailer.default_url_options = { protocol: 'https', host: host }
   config.action_mailer.raise_delivery_errors = true
